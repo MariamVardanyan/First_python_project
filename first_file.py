17727ac1d2fad43788cebdc497a49739a172b1e4
Mariam <mariam.2000.vardanyan@gmail.com>
2026-04-25 06:51:15 -0700

    merge changes from GitHub

--- first_file.py
+++ first_file.py
@@ -1,2 +1,23 @@
 #First
-print('hello')
+# import re
+# sentence = input('Please enter a sentence').lower()
+# clean = re.sub(r"[^a-z0-9]", "", sentence)
+# reverse = clean[::-1]   
+# if clean == reverse:
+#     return True
+# return False
+
+
+
+#Second
+ls = [5,4,5,4,6,4]
+d = {}
+
+for i in ls:
+    if i in d:
+        d[i] +=1
+    else:
+        d[i]=1
+for key,value in d.items():
+    if value == 1:
+        print(key)
